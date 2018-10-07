db2e24e55e2151bc4c4255085c41ab9e6bff26f9
Дарья Фурзикова <dfurzikova@yandex.ru>
2018-10-07 14:43:26 +0300

    Шаблон thermal

--- index.js
+++ index.js
@@ -31,53 +31,79 @@ var page = {
     },
 
     getEventType: function (events) {
+        var _this = this;
         if (!events) {
             return;
         }
 
-        events.forEach(this.fillTemplate.bind(this));
+        events.forEach(function(event){
+            var container = document.querySelector('.page-content-container');
+            var domNode = _this.fillTemplate('card-item', event);
+
+            if (domNode) {
+                container.appendChild(domNode);
+            }
+        });
     },
 
-    fillTemplate: function (event) {
-        var container = document.querySelector('.page-content-container');
-        var template = document.getElementById('card-item');
+    fillTemplate: function (name, data) {
+        var template = document.getElementById(name);
+
+        if (!template) {
+            return;
+        }
+
         var content = template.content.cloneNode(true);
         var domNode = content.querySelector('*');
-        var data= domNode.querySelectorAll('*[data-tmpl]');
-        var icon = domNode.querySelector('.card-item__icon');
-        var dataField;
+        var dataTmpl = domNode.querySelectorAll('*[data-tmpl]');
         
-        domNode.classList.add(
-            'card-item_size_' + event.size,
-            'card-item_type_' + event.type
-            );
-        icon.src = 'assets/' + event.icon + '.svg'
-       
-        data.forEach(function (d) {
-            dataField = d.dataset.tmpl;
-
-            d.innerHTML = event[dataField];
+        dataTmpl.forEach(function (d) {
+            var dataField = d.dataset.tmpl;
+            d.innerHTML = data[dataField];
+          //  console.log(d);
         })
-        
-        container.appendChild(domNode);
+
+        var dataTemplate = domNode.querySelectorAll('*[data-template]');
+
+        dataTemplate.forEach(function(node){
+            var newNode = this.fillTemplate(data.icon, data.data);
+
+            if (newNode) {
+                node.parentNode.replaceChild(newNode, node);
+            } else {
+                node.parentNode.removeChild(node);
+            }
+        }.bind(this));
+
+        if (this['template_' + name]) {
+            return this['template_' + name](domNode, data);
+        }
+    },
+    
+    'template_card-item': function(domNode, data) {
+        var icon = domNode.querySelector('.card-item__icon');
+       
+        domNode.classList.add(
+            'card-item_size_' + data.size,
+            'card-item_type_' + data.type
+            );
+        icon.src = 'assets/' + data.icon + '.svg'
+
+        return domNode;
+    },
+
+    'template_thermal': function(domNode, data) {
+       // console.log(domNode);
+        console.log (data, 'data');
+        //console.log (data.data.temrature, 'data.data.teerature' )
+        var temp = domNode.querySelector('.temperature');
+        var humidity = domNode.querySelector('.humidity');
+        temp.innerHTML = data.temperature + ' C';
+        humidity.innerHTML = data.humidity + ' %';
+        return domNode;
     }
 
 
+
+
 }
-
-
-
-
-// data.forEach(function (v) {
-//     var dataField = v.dataset.tmpl;
-//     var vData = data[dataField];
-//     console.log(vData);
-// })
-
-
-// // elem.querySelector('.card-item__title').innerHTML = '123';   
-// // t.content.querySelector('img').src = 'logo.png';
-
-
-
-
