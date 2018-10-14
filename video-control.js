fcf408fb56535b5460f7aae82e691b0c52ea62c0
Дарья Фурзикова <dfurzikova@yandex.ru>
2018-10-14 08:51:50 +0300

    Разметка, стили и методы для работы с потоковыми видео, анимация, фильтры и анализатор громкости

--- video-control.js
+++ video-control.js
@@ -1,38 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
     videoPage.init();
-    // events.init
 });
 
 var videoPage = {
+    filters: {},
+
     init: function () {
         this.getVideo();
+        this.bindEvents();
     },
 
-    getVideo: function(){
-        this.initVideo(
-            document.getElementById('video-1'),
-            'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fsosed%2Fmaster.m3u8'
-        );
-    
-        this.initVideo(
-            document.getElementById('video-2'),
-            'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fcat%2Fmaster.m3u8'
-        );
-    
-        this.initVideo(
-            document.getElementById('video-3'),
-            'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fdog%2Fmaster.m3u8'
-        );
-    
-        this.initVideo(
-            document.getElementById('video-4'),
+    getVideo: function () {
+        var videos = [
+            'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fsosed%2Fmaster.m3u8',
+            'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fcat%2Fmaster.m3u8',
+            'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fdog%2Fmaster.m3u8',
             'http://localhost:9191/master?url=http%3A%2F%2Flocalhost%3A3102%2Fstreams%2Fhall%2Fmaster.m3u8'
-        );
+        ];
+
+        for (var i = 0; i < videos.length; i++) {
+            this.initVideo(document.getElementById('video-' + i), videos[i]);
+        }
     },
 
     initVideo: function (video, url) {
+        var hls;
+
         if (Hls.isSupported()) {
-            var hls = new Hls();
+            hls = new Hls();
+
             hls.loadSource(url);
             hls.attachMedia(video);
             hls.on(Hls.Events.MANIFEST_PARSED, function () {
@@ -42,8 +38,162 @@ var videoPage = {
             video.src = 'https://video-dev.github.io/streams/x36xhzz/x36xhzz.m3u8';
             video.addEventListener('loadedmetadata', function () {
                 video.play();
-            })
+            });
         }
     },
 
+    bindEvents: function () {
+        var container = document.querySelector('.video-content');
+        var buttonBack = document.querySelector('.button__backward');
+        var controls = document.querySelector('.video-controls');
+        var inputBrightness = document.querySelector('.brightness');
+        var inputContrast = document.querySelector('.contrast');
+        var volume = document.querySelector('.button__volume');
+
+        container.addEventListener('click', this.showFullVideo.bind(this, controls));
+        buttonBack.addEventListener('click', this.hideVideo.bind(this, controls));
+        volume.addEventListener('click', this.toggleMute.bind(this));
+        inputBrightness.addEventListener('input', this.setFilters.bind(this));
+        inputContrast.addEventListener('input', this.setFilters.bind(this));
+    },
+
+    showFullVideo: function (controls, event) {
+        if (event.target.tagName !== 'VIDEO' || this.video === event.target) {
+            return;
+        }
+
+        event.preventDefault();
+        this.video = event.target;
+        
+        this.video.muted = 0;
+        volumeAnalizator.analyze(this.video);
+        this.video.play();
+
+
+        this.moveDom(this.video, this.video.parentNode, document.body)
+            .then(function () {
+                controls.style.display = 'flex';
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },
+
+    toggleMute: function () {
+        this.video.muted = !this.video.muted;
+    },
+
+    moveDom: function (dom, from, to, goHome) {
+        var _this = this;
+        var promise = new Promise(function (resolve) {
+            _this.setPosition(dom, from);
+
+            dom.style.transition = '';
+
+            window.requestAnimationFrame(function () {
+                _this.setPosition(dom, to);
+
+                dom.style.transition = 'all 1s';
+                setTimeout(resolve, 1010);
+            });
+        });
+
+        if (goHome) {
+            promise.then(function () {
+                dom.style.transition = '';
+                dom.style.position = '';
+                dom.style.width = '';
+                dom.style.height = '';
+                dom.style.left = '';
+                dom.style.top = '';
+            })
+        }
+
+        return promise;
+    },
+
+    setPosition: function (dom, to) {
+        var toRect = to.getBoundingClientRect();
+
+        dom.style.position = 'fixed';
+
+        if (to === document.body) {
+            dom.style.width = '100%';
+            dom.style.height = '100%';
+            dom.style.left = 0;
+            dom.style.top = 0;
+        } else {
+            dom.style.width = toRect.width + 'px';
+            dom.style.height = toRect.height + 'px';
+            dom.style.left = toRect.left + 'px';
+            dom.style.top = toRect.top + 'px';
+        }
+    },
+
+    hideVideo: function (controls) {
+        this.video.muted = 1;
+        controls.style.display = 'none';
+        this.moveDom(this.video, document.body, this.video.parentNode, true);
+    },
+
+    setFilters: function (e) {
+        var type = e.target.getAttribute('control');
+        var filters = this.filters;
+
+        filters[type] = e.target.value;
+
+        this.video.style.filter = Object.keys(filters).reduce(function (acc, key) {
+            return acc + key + '(' + filters[key] + '%) ';
+        }, '');
+    }
+};
+
+var volumeAnalizator = {
+    contexts: {},
+
+    analyze: function (video) {
+        if (video.haveContext) {
+            this.analyser = video.analyser;
+            
+            return;
+        }
+
+        var AudioContext = window.AudioContext || window.webkitAudioContext;
+
+        this.context = new AudioContext();
+        video.haveContext = true;
+
+        this.analyser = video.analyser = this.context.createAnalyser();
+        this.analyser.smoothingTimeConstant = 0.3;
+        this.analyser.fftSize = 512;
+
+        this.source = this.context.createMediaElementSource(video);
+        this.source.connect(this.analyser);
+        this.source.connect(this.context.destination);
+
+        this.node = this.context.createScriptProcessor(512, 1, 1);
+        this.node.connect(this.context.destination);
+        this.analyser.connect(this.node);
+
+        this.node.addEventListener('audioprocess', this.getVolumeValue.bind(this));
+    },
+
+    getVolumeValue: function () {
+        var array = new Uint8Array(this.analyser.frequencyBinCount);
+        var currentVolume;
+
+        this.analyser.getByteFrequencyData(array);
+
+        currentVolume = array.reduce(function (a, b) {
+            return a > b ? a : b
+        });
+
+        this.drowValue(currentVolume);
+    },
+
+    drowValue: function (value) {
+        var volumeBackground = document.querySelector('.video-controls__background');
+
+        volumeBackground.style.width = (value / 255 * 100) + '%';
+    }
 }
