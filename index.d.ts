0d457320c6e514bdad08e523a7e8759ee5360389
Дарья Фурзикова <dfurzikova@yandex.ru>
2018-10-25 12:15:40 +0300

    Заголовочный файл для index.ts

---
+++ index.d.ts
@@ -0,0 +1,71 @@
+interface HTMLElement {
+    content: Element;
+}
+
+interface Node {
+    querySelector<E extends Element = Element>(selectors: string): E | null;
+}
+
+interface Event {
+    clientX: number;
+    clientY: number;
+    x: number;
+}
+
+
+
+interface Page {
+    templates: { 
+        [key: string]: Template
+    };
+    getDataEvents(): Promise<FetchData>;
+}
+
+interface Template {
+    (domNode: HTMLElement, data: TemplateData): HTMLElement;
+}
+
+interface FetchData {
+    events: TemplateData[];
+}
+
+interface TemplateData {
+    [key:string]: 
+        string | 
+        TemplateData | 
+        string[];
+    type: string;
+    title: string;
+    source: string;
+    description: string;
+    icon: string;
+    size: string;
+    buttons: string[];
+    data: TemplateData;
+}
+
+interface PointerEventsDom {
+    pointers: { 
+        [key: number]: Event
+    };
+
+    currentImageX: number;
+    currentZoom: number;
+    startDistance: number;
+    currentStartX: number;
+    currentPointerX: number;
+    currentDistance: number;
+    lastZoom: number;
+    pointerArr: number[];
+
+    bindEvents(): void;
+    onPointerDown(image: HTMLElement, event: PointerEvent): void;
+    getXPoint(): number;
+    getDistance(): number;
+    bindEvents(): void;
+
+    onPointerMove(image: HTMLElement, event: PointerEvent): void;
+    onPointerUp(event: PointerEvent): void;
+    directionX(image: HTMLElement): void;
+    pinchZoom(image: HTMLElement, event: PointerEvent): void;
+}
